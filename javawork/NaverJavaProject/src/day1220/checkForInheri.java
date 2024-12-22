e9cb07a8965cfd345cd870f0878dee3558bfc3d9
1suckk <cwsgcd@gmail.com>
2024-12-22 18:53:39 +0900

    1222 update

--- javawork/NaverJavaProject/src/day1220/checkForInheri.java
+++ javawork/NaverJavaProject/src/day1220/checkForInheri.java
@@ -1,45 +1,50 @@
-public class A
+package day1220;
+
+class A
 {
-  public void method1()
-  {
-    System.out.println("A-method1()");
-  }
+	public void method1()
+	{
+		System.out.println("A-method1()");
+	}
 }
 
-public class B extends A
+class B extends A
 {
-  public void method1()
-  {
-    System.out.println("B-method1()");
-  }
+	public void method1()
+	{
+		System.out.println("B-method1()");
+	}
 }
 
-public class C extends A
+class C extends A
 {
-  public void method1()
-  {
-    System.out.println("C-method1()");
-  }
+	public void method1()
+	{
+		System.out.println("C-method1()");
+	}
 
-  public void method2()
-  {
-    System.out.println("C-method2()");
-  }
+	public void method2()
+	{
+		System.out.println("C-method2()");
+	}
 }
 
+/*
+ * 자바 12부터 instanceof를 쓰면서, 뒤에 곧바로 자식 클래스와
+ * 인스턴스를 사용하는 것이 가능해진다 
+ */
 public class checkForInheri {
-  public void method1(A a) {
-    a.method1();
-    if (a == c)
-    {
-      c.method2();
-    }
-  }
+	public static void action(A a) {
+		a.method1();
+		if (a instanceof C c) {
+			c.method2();    
+		}
+	}
 
-  public static void main(String[] args) 
-  {
-    action(new A());
-    action(new B());
-    action(new C());
-  }
+	public static void main(String[] args) 
+	{
+		action(new A());
+		action(new B());
+		action(new C());
+	}
 } 
